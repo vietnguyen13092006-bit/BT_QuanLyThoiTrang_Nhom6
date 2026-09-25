d64dd127207cbbf96e01a1096170058bbdfdeff2
vietnguyen13092006-bit <vietnguyen13092006@gmail.com>
2026-09-25 15:57:43 +0700

    Add files via upload

--- DATABASE/SQP_TAIKHOAN.sql
+++ DATABASE/SQP_TAIKHOAN.sql
@@ -7,8 +7,13 @@ CREATE TABLE Users(
 	Userid INT IDENTITY(1,1) PRIMARY KEY,
 	Username NVARCHAR(50) NOT NULL,
 	PassW VARCHAR(250) NOT NULL,
+	SDT VARCHAR(15) null,
 	Email NVARCHAR(100) null
 );
+
+GO
+ALTER TABLE Users
+ADD AvatarPath NVARCHAR(255) null;
 GO
 
-SELECT* FROM Users
+select* from Users
